bffc394ae485db22d1a1436d7837f12ae3ae3ef3
Aidan Feldman <aidan.feldman@gmail.com>
2011-09-14 18:21:04 -0300

    apparently File joins can't take Arrays... join the exploded path first

--- lib/stache/asset_helper.rb
+++ lib/stache/asset_helper.rb
@@ -7,7 +7,7 @@ module Stache
         exploded = source.split("/")
         file = exploded.pop
         file = file.split(".").first
-        template_path = ::Rails.root.join('app/views', exploded, "_#{file}.html.mustache")
+        template_path = ::Rails.root.join('app/views', exploded.join('/'), "_#{file}.html.mustache")
         template = ::File.open(template_path, "rb")
         content_tag(:script, template.read.html_safe, :type => "text/html", :id => "#{file.dasherize.underscore}_template")
       end.join("\n").html_safe

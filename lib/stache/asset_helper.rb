b3cef1e5983d49c12bbfb8f5f94d5feab63d75c1
Matt Wilson <mhw@hypomodern.com>
2011-10-26 09:18:16 -0400

    Update AssetHelper to search for templates in more places and raise an appropriate error if one cannot be found.

--- lib/stache/asset_helper.rb
+++ lib/stache/asset_helper.rb
@@ -7,10 +7,29 @@ module Stache
         exploded = source.split("/")
         file = exploded.pop
         file = file.split(".").first
-        template_path = Stache.template_base_path.join(exploded.join("/"), "_#{file}.html.mustache")
-        template = ::File.open(template_path, "rb")
-        content_tag(:script, template.read.html_safe, :type => "text/html", :id => "#{file.dasherize.underscore}_template")
+
+        base_path = Stache.template_base_path.join(*exploded)
+        template_path = locate_template_for(base_path, file)
+        if template_path
+          template = ::File.open(template_path, "rb")
+          content_tag(:script, template.read.html_safe, :type => "text/html", :id => "#{file.dasherize.underscore}_template")
+        else
+          raise ActionView::MissingTemplate.new(potential_paths(base_path, file), file, [base_path], false, { :handlers => [:mustache] })
+        end
       end.join("\n").html_safe
+    end
+    
+    def potential_paths(path, candidate_file_name)
+      [
+        path.join("_#{candidate_file_name}.html.mustache"),
+        path.join("_#{candidate_file_name}.mustache"),
+        path.join("#{candidate_file_name}.html.mustache"),
+        path.join("#{candidate_file_name}.mustache")
+      ]
+    end
+    
+    def locate_template_for(path, candidate_file_name)
+      potential_paths(path, candidate_file_name).find { |file| File.file?(file.to_s) }
     end
   end
 end
--- spec/stache/asset_helper_spec.rb
+++ spec/stache/asset_helper_spec.rb
@@ -10,8 +10,9 @@ describe Stache::AssetHelper do
     @helper ||= MyViewContext.new
   end
   
-  describe "template_include_tag" do
+  describe "#template_include_tag" do
     it "renders a script tag with the template contents" do
+      File.stub!(:file?).with(Rails.root.join("app/views/widgets/_oh_herro.html.mustache").to_s).and_return(true)
       File.stub!(:open).with(Rails.root.join("app/views/widgets/_oh_herro.html.mustache"), "rb").
         and_return(StringIO.new("{{ awyeah }}"))
 
@@ -21,10 +22,28 @@ describe Stache::AssetHelper do
       Stache.configure do |c|
         c.template_base_path = "/tmp/whee"
       end
+      File.stub!(:file?).with("/tmp/whee/_whooo.html.mustache").and_return(true)
       File.stub!(:open).with(Pathname.new("/tmp/whee/_whooo.html.mustache"), "rb").
         and_return(StringIO.new("{{ awyeah }}"))
         
       helper.template_include_tag("whooo").should == "<script id=\"whooo_template\" type=\"text/html\">{{ awyeah }}</script>"
+    end
+    it "raises if it cannot find the template" do
+      -> { helper.template_include_tag("arrrgh") }.should raise_error(ActionView::MissingTemplate)
+    end
+  end
+  
+  describe "#locate_template_for" do
+    it "tries permutations of partial names and file extensions to find the requested file" do
+      File.should_receive(:file?).with("/tmp/whee/_whooo.html.mustache")
+      File.should_receive(:file?).with("/tmp/whee/_whooo.mustache")
+      File.should_receive(:file?).with("/tmp/whee/whooo.html.mustache")
+      File.should_receive(:file?).with("/tmp/whee/whooo.mustache").and_return(true)
+      
+      helper.locate_template_for(Pathname.new("/tmp/whee"), "whooo").should == Pathname.new("/tmp/whee/whooo.mustache")
+    end
+    it "returns nil if it cannot find anything" do
+      helper.locate_template_for(Pathname.new("/tmp/whee"), "whooo").should be_nil
     end
   end
 end

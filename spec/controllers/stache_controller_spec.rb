5ddbdf736fc2411955f2534c68f0dce81056446d
Matt Wilson <mhw@hypomodern.com>
2012-04-10 09:33:13 -0400

    Hack: the controller spec assumes that the dummy app's config is active, but it might not be. Fix it like a hack.

--- spec/controllers/stache_controller_spec.rb
+++ spec/controllers/stache_controller_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 describe StacheController do
   render_views
-  
+
+  before do
+    Stache.template_base_path = ::Rails.root.join('app', 'views')
+  end
+
   it "can get to index and render a Mustache" do
     get :index
     assert_response 200
@@ -10,14 +14,14 @@ describe StacheController do
     response.should render_template 'index'               # view
     response.body.should =~ /Hello, Matt!/
   end
-  
+
   it "correctly renders partials" do
     get :with_partials
     assert_response 200
-    
+
     response.body.should =~ /Grue/
     # puts response.body
   end
-  
-  
+
+
 end

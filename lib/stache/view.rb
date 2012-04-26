803be80b2e09a1fb00e0ceea589f77fd0b0de5a1
Aaron Westendorf <aaron.westendorf@gmail.com>
2012-04-25 20:47:55 -0400

    Search for partials with and without leading underscore

--- lib/stache/view.rb
+++ lib/stache/view.rb
@@ -20,18 +20,37 @@ module Stache
     #
     # (1) in the same directory as the current template file.
     # (2) in the shared templates path (can be configured via Config.shared_path=(value))
+    # (3) with or without leading underscore
     #
     def partial(name)
-      partial_name = "_#{name}.#{Stache.template_extension}"
       template_dir = self.virtual_path.split("/")[0..-2].join("/")
-      partial_path = File.expand_path(File.join(Stache.template_base_path, template_dir, partial_name))
+      template_path = File.join(Stache.template_base_path, template_dir)
+      partial_path = locate_partial_for(template_path, name)
+
       # ::Rails.logger.info "Checking for #{partial_path} in template_dir: #{template_dir}"
-      unless File.file?(partial_path)
-        partial_path = "#{Stache.shared_path}/#{partial_name}"
+      if partial_path.nil?
+        partial_path = locate_partial_for(Stache.shared_path, name)
+        
+        if partial_path.nil?
+          failed_paths = potential_paths(template_path,name) + potential_paths(Stache.shared_path, name) 
+          raise ActionView::MissingTemplate.new(failed_paths, name, [template_path, Stache.shared_path], true, { :handlers => [:mustache] })
+        end
+
       end
       
       # ::Rails.logger.info "LOADING PARTIAL: #{partial_path}"
       File.read(partial_path)
+    end
+    
+    def potential_paths(path, candidate_file_name)
+      [
+        File.join(path, "_#{candidate_file_name}.#{Stache.template_extension}"),
+        File.join(path, "#{candidate_file_name}.#{Stache.template_extension}")
+      ]
+    end
+    
+    def locate_partial_for(path, candidate_file_name)
+      potential_paths(path, candidate_file_name).find { |file| File.file?(file.to_s) }
     end
     
   end

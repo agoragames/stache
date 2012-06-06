0ede636d1be865f2ee6d35b7489fbb54da65f5e1
Jeremy Bush <contractfrombelow@gmail.com>
2012-06-06 17:15:47 -0500

    Load mustache classes before mustache templates. This allows the feature of the class determining the template instead of rails, which is the proper way to use mustache

--- lib/stache/mustache.rb
+++ lib/stache/mustache.rb
@@ -4,4 +4,5 @@ module Stache
   module Mustache; end
 end
 
+ActionView::Template.register_template_handler :rb, Stache::Mustache::Handler
 ActionView::Template.register_template_handler :mustache, Stache::Mustache::Handler

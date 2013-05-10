4e4c22f18bcb32fa431af62ff240afe7b89bfcea
joker1007 <kakyoin.hierophant@gmail.com>
2013-05-10 17:27:09 +0900

    Change additional path from Pathname to String
    Because some gems expects that autoload_paths is Array of String

--- lib/stache/railtie.rb
+++ lib/stache/railtie.rb
@@ -1,7 +1,7 @@
 module Stache
   class Railtie < ::Rails::Railtie
     initializer 'stache.autoload', :before => :set_autoload_paths do |app|
-      app.config.autoload_paths << Rails.root + 'app/views'
+      app.config.autoload_paths << (Rails.root + 'app/views').to_s
     end
 
     config.to_prepare do
